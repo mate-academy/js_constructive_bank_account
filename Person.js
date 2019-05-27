9d2cc72a7ae39c0f2d3a91373604a0671e49e837
VladimirTi <tytmes@mail.ru>
2019-05-28 00:27:07 +0300

    Vladimir's Solution

--- Person.js
+++ Person.js
@@ -1,29 +1,30 @@
-class Person {
-  constructor(name, dateOfBirth, amount) {
-    this.name = name;
-    this.dateOfBirth = dateOfBirth;
-    this.amount = amount;
-    this.date = new Date;
-    this.age = this.date.getFullYear() - this.dateOfBirth.slice(-4) - 1;
-    this.history = [`Initial: ${this.amount}`];
-  }
+function Person (name, dateOfBirth, amount) {
+  this.name = name;
+  this.dateOfBirth = dateOfBirth;
+  this.amount = amount;
+  this.date = new Date;
+  this.age = this.date.getFullYear() - this.dateOfBirth.slice(-4) - 1;
+  this.history = [`Initial: ${this.amount}`];
+}
 
-  getInfo() {
+Person.prototype = {
+  getInfo: function() {
     console.log(`Name: ${this.name}, Age: ${this.age}, Amount: ${this.amount}$.`);
-  }
+  },
 
-  addMoney(receipts, comment) {
+  addMoney: function(receipts, comment) {
     this.amount += receipts;
     this.history.push(`${comment}: ${receipts}`);
-  }
-  withdrawMoney(withdraw, comment) {
+  },
+
+  withdrawMoney: function(withdraw, comment) {
     this.amount -= withdraw;
     this.history.push(`${comment}: -${withdraw}`);
-  }
+  },
 
-  getAccountHistory() {
+  getAccountHistory: function () {
     console.log(this.history);
-  }
-};
+  },
+}
 
 module.exports = Person;
