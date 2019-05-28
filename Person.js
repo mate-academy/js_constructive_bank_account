87740723f5e73056d5523bfe5bcf74f62f846044
VladimirTi <tytmes@mail.ru>
2019-05-28 16:34:48 +0300

    Vladimir's Solution

--- Person.js
+++ Person.js
@@ -1,29 +1,32 @@
-function Person (name, dateOfBirth, amount) {
+function Person(name, birth, amount) {
   this.name = name;
-  this.dateOfBirth = dateOfBirth;
   this.amount = amount;
-  this.date = new Date;
-  this.age = this.date.getFullYear() - this.dateOfBirth.slice(-4) - 1;
+  this.age = this.getDate(birth)
   this.history = [`Initial: ${this.amount}`];
 }
 
 Person.prototype = {
-  getInfo: function() {
+  getInfo: function () {
     console.log(`Name: ${this.name}, Age: ${this.age}, Amount: ${this.amount}$.`);
   },
 
-  addMoney: function(receipts, comment) {
+  addMoney: function (receipts, comment) {
     this.amount += receipts;
     this.history.push(`${comment}: ${receipts}`);
   },
 
-  withdrawMoney: function(withdraw, comment) {
+  withdrawMoney: function (withdraw, comment) {
     this.amount -= withdraw;
     this.history.push(`${comment}: -${withdraw}`);
   },
 
   getAccountHistory: function () {
     console.log(this.history);
+  },
+
+  getDate: function (date) {
+    const arr = date.split('.')
+    return years = new Date(new Date() - new Date(`${arr[2]}/${arr[1]}/${arr[0]}`)).getFullYear() - 1970;
   },
 }
 
