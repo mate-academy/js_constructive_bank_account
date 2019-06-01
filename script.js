98b60fb43783aa84674e0138c383e3026ea7b2f2
Alexandr <alexandrmail32@gmail.com>
2019-06-01 21:22:19 +0300

    remove comments

--- script.js
+++ script.js
@@ -23,8 +23,9 @@ function Person(name, birth, money) {
       }
     
   this.__calculateAge = function() {
-        const birthArray = this.birthday.split('.');
-        const age = new Date(Date.now() - +new Date(birthArray[2], birthArray[1], birthArray[0])).getFullYear() -  new Date(0).getFullYear();
+    const [day, month, year] = this.birthday.split('.');
+    const age = new Date(Date.now() - +new Date(year, month, day)).getFullYear() -  new Date(0).getFullYear();
+    
         
         return age;
       }
@@ -33,10 +34,10 @@ function Person(name, birth, money) {
 const dmytro = new Person('Dmytro', '26.11.1994', 1000);
 const pavel = new Person('Pavel', '06.06.1990', 400);
 
-dmytro.getInfo(); // print `Name: Dmytro, Age: 24, Amount: 1000$`
+dmytro.getInfo(); 
 dmytro.addMoney(2000, 'salary');
 dmytro.withdrawMoney(500, 'new phone');
-dmytro.getInfo(); // Name: Dmytro, Age: 24, Amount: 2500$
+dmytro.getInfo(); 
 dmytro.withdrawMoney(500, 'apartment rent');
-dmytro.getAccountHistory(); // [ 'Initial: 1000', 'salary: 2000', 'new phone: -500', 'apartment rent: -500']
-pavel.getInfo(); // // Name: Pavel, Age: 28, Amount: 400$
+dmytro.getAccountHistory(); 
+pavel.getInfo(); 
