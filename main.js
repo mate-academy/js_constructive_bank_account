7b7d6ac4d9a42bb0fac2ea9e5a2d1056507539c1
Dmitry Batsenko <batsenkod@gmail.com>
2019-06-03 08:24:40 +0300

    Fixed

--- main.js
+++ main.js
@@ -1,46 +1,39 @@
 function Person(name, date, money) {
-    this.name = name;
-    this.date = date;
-    this.money = 0;
-    this.history = [];
-  
-    this.getAge = () => {
-      let dateold = new Date(this.date.replace( /(\d{2}).(\d{2}).(\d{4})/, "$2/$1/$3"));
-      let datenew = new Date(Date.now());
-      let ynew = datenew.getFullYear();
-      let mnew = datenew.getMonth();
-      let dnew = datenew.getDate();
-      let yold = dateold.getFullYear();
-      let mold = dateold.getMonth();
-      let dold = dateold.getDate();
-      let diff = ynew - yold;
-      if (mold > mnew) diff--;
-      else {
-        if (mold == mnew) {
-          if (dold > dnew) diff--;
-        }
-      }
-      return diff;
-    }
-    
-    this.getInfo = () => {
-      console.log(`Name: ${this.name}, Age: ${this.getAge()}, Amount: ${this.money}$`);
-    };
-    
-    this.addMoney = (count, text) => {
-      this.money += count;
-      this.history.push(`${text}: ${count}$`);
-    };
-    
-    this.withdrawMoney = (count, text) => {
-      this.money -= count;
-      this.history.push(`${text}: -${count}$`);
-    };
-    
-    this.getAccountHistory = () => this.history;
-    
-    this.addMoney(money, "Initial");
+  this.name = name;
+  this.date = date;
+  this.money = 0;
+  this.history = [];
+
+  this.addMoney(money, "Initial");
 }
+
+Person.prototype.getAge = function () {
+  let birthdate = new Date(this.date.replace(/(\d{2}).(\d{2}).(\d{4})/, "$2/$1/$3"));
+  let cur = new Date();
+  let diff = cur - birthdate;
+
+  return Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+}
+
+Person.prototype.getInfo = function () {
+  return `Name: ${this.name}, Age: ${this.getAge()}, Amount: ${this.money}$`;
+};
+
+Person.prototype.addMoney = function (count, text) {
+  this.money += count;
+  this.addHistory(text, count);
+};
+
+Person.prototype.withdrawMoney = function (count, text) {
+  this.money -= count;
+  this.addHistory(text, -count);
+};
+
+Person.prototype.addHistory = function (text, count) {
+  this.history.push(`${text}: ${count}$`);
+}
+
+Person.prototype.getAccountHistory = function () { return this.history };
 
 const dmytro = new Person('Dmytro', '26.11.1994', 1000);
 const pavel = new Person('Pavel', '06.06.1990', 400);
