1c914aedcba01d240cdaf9f075a32e1ead8a0109
annakondr <tyukondra@gmail.com>
2019-06-04 19:44:44 +0300

    solution_with_requested_changes

--- src/main.js
+++ src/main.js
@@ -1,46 +1,46 @@
 'use strict';
 function Person(name, dateOfBirth, amount) {
-    this.name = name;
-    this.dateOfBirth = dateOfBirth.split('.').reverse();
-    this.amount = amount;
+  this.name = name;
+  this.amount = amount;
+  let [day, month, year] = dateOfBirth.split('.');
 
-    let [year, month, day] = this.dateOfBirth;
-
-    this.age = function getAge() {
-        const currentYear = new Date().getFullYear();
-        const currentMonth = new Date().getMonth() + 1;
-        const currentDay = new Date().getDate();
-        if (currentMonth < month || currentMonth === month && currentDay < day) {
-            return currentYear - year + 1;
-        }
-        return currentYear - year;
-    };
-
-    this.currentAccount = this.amount;
-    this.accountHistory = [{'initial': amount}];
-    this.transaction = function(value, info) {
-        this.accountHistory.push({[info]: value})
+  this.age = function getAge() {
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const currentMonth = date.getMonth() + 1;
+    const currentDay = date.getDate();
+    const age = currentYear - year;
+    if (currentMonth < month || currentMonth === month && currentDay < day) {
+      return age + 1;
     }
+    return age;
+  };
+
+  this.currentAccount = this.amount;
+  this.accountHistory = [{'initial': amount}];
+  this.transaction = function(value, info) {
+    this.accountHistory.push({[info]: value})
+  }
 
 }
 
 Person.prototype.getInfo = function() {
-    console.log(`Name: ${this.name}, Age: ${this.age()}, Amount: ${this.amount}`)
+  return (`Name: ${this.name}, Age: ${this.age()}, Amount: ${this.amount}`)
 };
 
 
 Person.prototype.addMoney = function(income, info) {
-    this.currentAccount += income;
-    this.transaction(income, info)
+  this.currentAccount += income;
+  this.transaction(income, info)
 };
 
 Person.prototype.withdrawMoney = function(outgo, info) {
-    this.currentAccount -= outgo;
-    this.transaction(`-${outgo}`, info)
+  this.currentAccount -= outgo;
+  this.transaction(`-${outgo}`, info)
 };
 
 Person.prototype.getAccountHistory = function() {
-    console.log(this.accountHistory);
+  return (this.accountHistory);
 };
 
 const dmytro = new Person('Dmytro', '26.11.1994', 1000);
