aa317095e0214c1dec141a6c72427fb38165cad7
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-10-10 21:48:53 +0300

    Update bank_account.js

--- bank_account.js
+++ bank_account.js
@@ -2,32 +2,31 @@ function Person(name, birthDate, amount) {
   this.name = name;
   this.birthDate = birthDate;
   this.amount = amount;
-  this.age = getAge(this.birthDate);
   this.accountHistory = [{name: 'initial', value: this.amount}];
 }
 
-function getAge(birthDate) {
+Person.prototype.pushItems = function(accountHistory, amount, source) {
+  accountHistory.push({name: source, value: amount});
+}
+
+Person.prototype.getAge = function(birthDate) {
   birthDate = birthDate.split('.').reverse().join('.');
   let age = Math.abs(new Date(Date.now() - Date.parse(birthDate)).getUTCFullYear() - 1970);
   return age;
 }
 
-function pushItems(accountHistory, amount, source) {
-  accountHistory.push({name: source, value: amount});
-}
-
 Person.prototype.getInfo = function() {
-  return (`Name: ${this.name}, Age: ${this.age}, Amount: ${this.amount}\$`);
+  return (`Name: ${this.name}, Age: ${this.getAge(this.birthDate)}, Amount: ${this.amount}\$`);
 }
 
 Person.prototype.addMoney = function(amount, source) {
   this.amount += amount;
-  pushItems(this.accountHistory, amount, source);
+  this.pushItems(this.accountHistory, amount, source);
 }
 
 Person.prototype.withdrawMoney = function(amount, source) {
   this.amount -= amount;
-  pushItems(this.accountHistory, -amount, source);
+  this.pushItems(this.accountHistory, -amount, source);
 }
 
 Person.prototype.getAccountHistory = function() {
