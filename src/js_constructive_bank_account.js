9f8f229d754fcd4ebcd8d8e89669d611e744eaf0
YuraBobeshko <gorobech55@gmail.com>
2019-10-11 14:39:14 +0300

    sol

--- src/js_constructive_bank_account.js
+++ src/js_constructive_bank_account.js
@@ -46,14 +46,3 @@ Person.prototype.withdrawMoney = function(sum, add) {
   this.money -= sum;
   this.addLog('-' + sum, add)
 };
-
-const dmytro = new Person("Dmytro", "26.11.1994", 1000);
-const pavel = new Person("Pavel", "06.06.1990", 400);
-
-dmytro.getInfo(); // print `Name: Dmytro, Age: <calculate yourself>, Amount: 1000$`
-dmytro.addMoney(2000, "salary");
-dmytro.withdrawMoney(500, "new phone");
-dmytro.getInfo(); // Name: Dmytro, Age: <calculate yourself>, Amount: 2500$
-dmytro.withdrawMoney(500, "apartment rent");
-dmytro.getAccountHistory(); // [ 'Initial: 1000', 'salary: 2000', 'new phone: -500', 'apartment rent: -500']
-pavel.getInfo(); // // Name: Pavel, Age: <calculate yourself>, Amount: 400$
