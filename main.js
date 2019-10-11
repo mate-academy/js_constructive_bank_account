f32f79f847f19c70cf8d921a3f2c85748d50b4c2
Sijey <popov040391@gmail.com>
2019-10-11 14:24:53 +0300

    Solution

--- main.js
+++ main.js
@@ -16,12 +16,7 @@ Person.prototype._calculateAge = function(dateOfBirth) {
 }
 
 Person.prototype.addItems = function(amount, notes) {
-    if (amount > 0) {
-        this.amount += amount;
-    }else {
-        this.amount -= amount;
-    }
-
+    this.amount += amount;
     this.history.push({name: notes, value: amount});
 }
 
@@ -54,8 +49,10 @@ const pavel = new Person('Pavel', '06.06.1990', 400);
 console.log(dmytro.getInfo()); // print `Name: Dmytro, Age: <calculate yourself>, Amount: 1000$`
 console.log(dmytro.addMoney(2000, 'salary'));
 console.log(dmytro.withdrawMoney(500, 'new phone'));
-console.log(dmytro.getInfo()); // Name: Dmytro, Age: <calculate yourself>, Amount: 2500$
+console.log(dmytro.getInfo());
 console.log(dmytro.withdrawMoney(500, 'apartment rent'));
+console.log(dmytro.getInfo());
 console.log(dmytro.getAccountHistory()); // [ 'Initial: 1000', 'salary: 2000', 'new phone: -500', 'apartment rent: -500']
+
 
 console.log(pavel.getInfo()); // // Name: Pavel, Age: <calculate yourself>, Amount: 400$
