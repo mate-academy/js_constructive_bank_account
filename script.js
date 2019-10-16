b93987c22d77c689ce59f410e6f9d62fe3269a47
olesyamogil <olesya.mogil@gmail.com>
2019-10-16 13:16:10 +0300

    Solution

--- script.js
+++ script.js
@@ -7,44 +7,47 @@ const Person = function(name, dateOfBirth, initialAmount) {
         );
     }
     this.amount = initialAmount;
-    this.history = [];
-    this.history.push(`Initial: ${initialAmount}`);
+    this.history = [{description: "Initial", amount: initialAmount}];
 };
 
-Person.prototype.getAge = function() {
+Person.prototype._getAge = function() {
     const month = this.dateOfBirth.getMonth();
     const day = this.dateOfBirth.getDate();
     const year = this.dateOfBirth.getFullYear();
-    let yearNow = new Date().getFullYear();
-    let monthNow = new Date().getMonth() + 1;
-    let dayNow = new Date().getDate();
-    if (monthNow === month && dayNow < day || monthNow < month) {
-        return yearNow - year - 1;
-    } else {
-        return yearNow - year;
-    }
+    const yearNow = new Date().getFullYear();
+    const monthNow = new Date().getMonth() + 1;
+    const dayNow = new Date().getDate();
+    return (monthNow === month && dayNow < day || monthNow < month) ? yearNow - year - 1 : yearNow - year;
 };
 
 Person.prototype.getInfo = function() {
-    console.log(`Name: ${this.name}, Age: ${this.getAge()}, Amount: ${this.amount}$`);
+    return `Name: ${this.name}, Age: ${this._getAge()}, Amount: ${this.amount}$`;
+};
+
+Person.prototype.addTransactionToHistory = function(transaction) {
+    this.history.push(transaction);
 };
 
 Person.prototype.addMoney = function(amount, description) {
     this.amount += amount;
-    this.history.push(`${description}: ${amount}`);
+    this.addTransactionToHistory({description: description, amount: amount});
 };
 
 Person.prototype.withdrawMoney = function(amount, description) {
     if (this.amount > amount) {
         this.amount -= amount;
-        this.history.push(`${description}: -${amount}`);
+        this.addTransactionToHistory({description: description, amount: amount});
     } else {
         console.log("Operation declined.");
     }
 };
 
 Person.prototype.getAccountHistory = function() {
-    console.log(this.history);
+    let result = [];
+    this.history.forEach(item => {
+        result.push(`${item.description}: ${item.amount}`);
+    });
+    return result;
 };
 
 const dmytro = new Person('Dmytro', '26.11.1994', 1000);
@@ -55,5 +58,5 @@ dmytro.addMoney(2000, 'salary');
 dmytro.withdrawMoney(500, 'new phone');
 dmytro.getInfo(); // Name: Dmytro, Age: <calculate yourself>, Amount: 2500$
 dmytro.withdrawMoney(500, 'apartment rent');
-dmytro.getAccountHistory(); // [ 'Initial: 1000', 'salary: 2000', 'new phone: -500', 'apartment rent: -500']
+console.log(dmytro.getAccountHistory()); // [ 'Initial: 1000', 'salary: 2000', 'new phone: -500', 'apartment rent: -500']
 pavel.getInfo(); // // Name: Pavel, Age: <calculate yourself>, Amount: 400$
