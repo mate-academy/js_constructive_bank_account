b4846849a1ebb41bd2eab84a008ac3f648bbb37e
alexander <earthhuman41@gmail.com>
2020-01-23 12:32:47 +0200

    added test cases

--- src/bankAccount.test.js
+++ src/bankAccount.test.js
@@ -9,21 +9,56 @@ test('Should return account info', () => {
     .toBe(`Name: Dmytro, Age: 26, Amount: 1000$`);
 });
 
-test('Should return info after money operation and accountHistory', () => {
+test('Should make money operations', () => {
+  const account = new BankAccount('Oleg', '26.09.1993', 200);
+
+  account.addMoney(2100, 'salary');
+  account.withdrawMoney(600, 'new phone');
+  account.withdrawMoney(700, 'products');
+  account.addMoney(400, 'lottery');
+
+  expect(account.getInfo())
+    .toBe(`Name: Oleg, Age: 27, Amount: 1400$`);
+});
+
+test('Should have initial history', () => {
+  const account = new BankAccount('Dmytro', '26.11.1994', 1000);
+
+  const history = account.getAccountHistory();
+
+  expect(history[0])
+    .toBe('Initial: 1000');
+
+  expect(typeof history)
+    .toBe('object');
+
+  expect(history.length)
+    .toBe(1);
+});
+
+test('Should change accountHistory', () => {
   const account = new BankAccount('Oleg', '26.09.1993', 1700);
 
   account.addMoney(2000, 'salary');
   account.withdrawMoney(500, 'new phone');
 
+  const history = account.getAccountHistory();
+
   expect(account.getInfo())
     .toBe(`Name: Oleg, Age: 27, Amount: 3200$`);
 
-  expect(account.getAccountHistory()[0])
+  expect(history[0])
     .toBe('Initial: 1700');
 
-  expect(account.getAccountHistory()[1])
+  expect(history[1])
     .toBe('salary: 2000');
 
-  expect(account.getAccountHistory()[2])
+  expect(history[2])
     .toBe('new phone: -500');
+
+  expect(typeof history)
+    .toBe('object');
+
+  expect(history.length)
+    .toBe(3);
 });
