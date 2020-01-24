d8fbd8dfa74e2589c7504fdafbaa37ee95c89075
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-01-24 17:09:45 +0200

    done

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,31 @@
 'use strict';
 
-function BankAccount() {
-  // Write your code here
+function BankAccount(name, initial) {
+  this.name = name;
+  this.amount = initial;
+  this.transactions = [`Initial: ${initial}`];
+};
+
+BankAccount.prototype.getInfo = function() {
+  return `Name: ${this.name}, Amount: ${this.amount}$`;
+};
+
+BankAccount.prototype.addMoney = function(sum, transactoin) {
+  const balance = this.amount + sum;
+
+  this.transactions = [...this.transactions, `${transactoin}: ${sum}`];
+  this.amount = balance;
+};
+
+BankAccount.prototype.getAccountHistory = function() {
+  return this.transactions;
+};
+
+BankAccount.prototype.withdrawMoney = function(sum, transactoin) {
+  const balance = this.amount - sum;
+
+  this.transactions = [...this.transactions, `${transactoin}: -${sum}`];
+  this.amount = balance;
 };
 
 module.exports = BankAccount;
