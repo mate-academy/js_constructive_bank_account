0a4cf79edb0f2aad041089bf4e18232db2af149b
LutsenkoAnV <48694313+LutsenkoAnV@users.noreply.github.com>
2020-01-24 16:22:51 +0200

    solition

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,30 @@
 'use strict';
 
-function BankAccount() {
-  // Write your code here
+function BankAccount(name, initial) {
+  this.name = name;
+  this.initial = initial;
+
+  const history = [`Initial: ${initial}`];
+
+  this.history = history;
+};
+
+BankAccount.prototype.getInfo = function() {
+  return `Name: ${this.name}, Amount: ${this.initial}$`;
+};
+
+BankAccount.prototype.addMoney = function(amount, info) {
+  this.initial = this.initial + amount;
+  this.history.push(`${info}: ${amount}`);
+};
+
+BankAccount.prototype.withdrawMoney = function(amount, info) {
+  this.initial = this.initial - amount;
+  this.history.push(`${info}: -${amount}`);
+};
+
+BankAccount.prototype.getAccountHistory = function() {
+  return this.history;
 };
 
 module.exports = BankAccount;
