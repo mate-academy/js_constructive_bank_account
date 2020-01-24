34e351e4ac7f5a11933c4de274a671d5c9264c48
masha <mariafil2107@gmail.com>
2020-01-24 16:40:50 +0200

    renamed totalAmount to amount

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -4,23 +4,23 @@ function BankAccount(name, initialAmount) {
   this.name = name;
   this.initialAmount = initialAmount;
 
-  this.totalAmount = initialAmount;
+  this.amount = initialAmount;
   this.transactions = [`Initial: ${this.initialAmount}`];
 };
 
 BankAccount.prototype = {
   getInfo: function() {
-    return `Name: ${this.name}, Amount: ${this.totalAmount}$`;
+    return `Name: ${this.name}, Amount: ${this.amount}$`;
   },
 
   addMoney: function(amount, source) {
     this.transactions.push(`${source}: ${amount}`);
-    this.totalAmount += amount;
+    this.amount += amount;
   },
 
   withdrawMoney: function(amount, purpose) {
     this.transactions.push(`${purpose}: -${amount}`);
-    this.totalAmount -= amount;
+    this.amount -= amount;
   },
 
   getAccountHistory: function() {
