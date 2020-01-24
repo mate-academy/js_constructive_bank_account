a19106efc4a1152f231d353312f338c5c2647f62
masha <mariafil2107@gmail.com>
2020-01-24 16:32:41 +0200

    bankAccount

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,31 @@
 'use strict';
 
-function BankAccount() {
-  // Write your code here
+BankAccount.prototype = {
+  getInfo: function() {
+    return `Name: ${this.name}, Amount: ${this.totalAmount}$`;
+  },
+
+  addMoney: function(amount, source) {
+    this.transactions.push(`${source}: ${amount}`);
+    this.totalAmount += amount;
+  },
+
+  withdrawMoney: function(amount, purpose) {
+    this.transactions.push(`${purpose}: -${amount}`);
+    this.totalAmount -= amount;
+  },
+
+  getAccountHistory: function() {
+    return this.transactions;
+  },
+};
+
+function BankAccount(name, initialAmount) {
+  this.name = name;
+  this.initialAmount = initialAmount;
+
+  this.totalAmount = initialAmount;
+  this.transactions = [`Initial: ${this.initialAmount}`];
 };
 
 module.exports = BankAccount;
