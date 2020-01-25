4d733069117239f8e8c6b2084dd676595ae6efd9
Natalia Lypovyk <cardinnata@gmail.com>
2020-01-25 13:59:03 +0200

    Bank Account

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,35 @@
 'use strict';
 
-function BankAccount() {
-  // Write your code here
+function BankAccount(name, initialAmount) {
+  this.name = name;
+  this.amount = initialAmount;
+  this.history = [`Initial: ${initialAmount}`];
+};
+
+BankAccount.prototype.getInfo = function() {
+  return `Name: ${this.name}, Amount: ${this.amount}$`;
+};
+
+BankAccount.prototype.addMoney = function(money, transaction) {
+  const summary = this.amount + money;
+
+  this.history.push(`${transaction}: ${money}`);
+  this.amount = summary;
+
+  return `${this.amount}, ${this.transaction}`;
+};
+
+BankAccount.prototype.withdrawMoney = function(money, transaction) {
+  const summary = this.amount - money;
+
+  this.history.push(`${transaction}: -${money}`);
+  this.amount = summary;
+
+  return `${this.amount}, ${this.transaction}`;
+};
+
+BankAccount.prototype.getAccountHistory = function() {
+  return this.history;
 };
 
 module.exports = BankAccount;
