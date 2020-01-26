4393f2c05048d4fe4cf20a6a766448490eaafa9c
Senya Volkov <formobileprima@gmail.com>
2020-01-26 19:38:01 +0200

    solution

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,30 @@
 'use strict';
 
-function BankAccount() {
-  // Write your code here
+function BankAccount(name, amount) {
+  this.name = name;
+  this.amount = amount;
+  this.history = [];
+  this.history.push(`Initial: ${this.amount}`);
+}
+
+BankAccount.prototype = {
+  getInfo: function() {
+    return `Name: ${this.name}, Amount: ${this.amount}$`;
+  },
+
+  addMoney: function(money, destination) {
+    this.amount += money;
+    this.history.push(`${destination}: ${money}`);
+  },
+
+  withdrawMoney: function(money, destination) {
+    this.amount -= money;
+    this.history.push(`${destination}: -${money}`);
+  },
+
+  getAccountHistory: function() {
+    return this.history;
+  },
 };
 
 module.exports = BankAccount;
