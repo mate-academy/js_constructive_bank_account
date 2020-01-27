08182759c380b2e3d6ab2c1f2489633894f064ff
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-01-27 12:16:41 +0200

    fixed

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -10,22 +10,18 @@ BankAccount.prototype.getInfo = function() {
   return `Name: ${this.name}, Amount: ${this.amount}$`;
 };
 
-BankAccount.prototype.addMoney = function(sum, transactoin) {
-  const balance = this.amount + sum;
-
-  this.transactions = [...this.transactions, `${transactoin}: ${sum}`];
-  this.amount = balance;
+BankAccount.prototype.addMoney = function(sum, transaction) {
+  this.transactions = [...this.transactions, `${transaction}: ${sum}`];
+  this.amount = this.amount + sum;
 };
 
 BankAccount.prototype.getAccountHistory = function() {
   return this.transactions;
 };
 
-BankAccount.prototype.withdrawMoney = function(sum, transactoin) {
-  const balance = this.amount - sum;
-
-  this.transactions = [...this.transactions, `${transactoin}: -${sum}`];
-  this.amount = balance;
+BankAccount.prototype.withdrawMoney = function(sum, transaction) {
+  this.transactions = [...this.transactions, `${transaction}: -${sum}`];
+  this.amount = this.amount - sum;
 };
 
 module.exports = BankAccount;
