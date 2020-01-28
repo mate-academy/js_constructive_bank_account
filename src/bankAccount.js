1592a511c89df9d89f52135194f1a3e1d673b9b2
ded-git <sakarailko@gmail.com>
2020-01-28 17:56:38 +0200

    thanks Ostap

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,27 @@
 'use strict';
 
-function BankAccount() {
-  // Write your code here
+function BankAccount(name, money) {
+  this.name = name;
+  this.money = money;
+  this.history = [`Initial: ${this.money}`];
+};
+
+BankAccount.prototype.getInfo = function() {
+  return `Name: ${this.name}, Amount: ${this.money}$`;
+};
+
+BankAccount.prototype.addMoney = function(money, description) {
+  this.money += money;
+  this.history.push(`${description}: ${money}`);
+};
+
+BankAccount.prototype.withdrawMoney = function(money, description) {
+  this.money -= money;
+  this.history.push(`${description}: -${money}`);
+};
+
+BankAccount.prototype.getAccountHistory = function() {
+  return this.history;
 };
 
 module.exports = BankAccount;
