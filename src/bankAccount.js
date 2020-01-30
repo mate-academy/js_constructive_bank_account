4c28d71d20465efdccc80a4c161e218472d05f5d
ronyshchenko <roma.onyshchenko@gmail.com>
2020-01-30 14:27:25 +0200

    five

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,27 +1,27 @@
 'use strict';
 
+function BankAccount(name, amount) {
+  this.name = name;
+  this.amount = amount;
+  this.transaction = [`Initial: ${this.amount}`];
+};
+
 BankAccount.prototype.getInfo = function() {
   return `Name: ${this.name}, Amount: ${this.amount}$`;
 };
 
 BankAccount.prototype.addMoney = function(amount, description) {
   this.amount += amount;
-  this.trnansaction.push(`${description}: ${amount}`);
+  this.transaction.push(`${description}: ${amount}`);
 };
 
 BankAccount.prototype.withdrawMoney = function(amount, description) {
   this.amount -= amount;
-  this.trnansaction.push(`${description}: -${amount}`);
+  this.transaction.push(`${description}: -${amount}`);
 };
 
 BankAccount.prototype.getAccountHistory = function() {
-  return this.trnansaction;
-};
-
-function BankAccount(name, amount) {
-  this.name = name;
-  this.amount = amount;
-  this.trnansaction = [`Initial: ${this.amount}`];
+  return this.transaction;
 };
 
 module.exports = BankAccount;
