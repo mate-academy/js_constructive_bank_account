2e8acf5ba0ee4fc25deb7a0e80bc5620948ed6e3
Igor Kulish <igormagnetic@gmail.com>
2020-04-02 19:08:56 +0300

    sol

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,29 @@
 'use strict';
 
-function BankAccount() {
-  // Write your code here
+function BankAccount(name, amount) {
+  this.name = name;
+  this.amount = amount;
+  this.result = [`Initial: ${amount}`]
+};
+
+BankAccount.prototype.getInfo = function() {
+  return `Name: ${this.name}, Amount: ${this.amount}$`;
+};
+
+BankAccount.prototype.addMoney = function(money, action) {
+  this.amount += money;
+  this.action = action;
+  this.result.push(`${this.action}: ${money}`);
+};
+
+BankAccount.prototype.withdrawMoney = function(money, action) {
+  this.amount -= money;
+  this.action = action;
+  this.result.push(`${this.action}: -${money}`);
+};
+
+BankAccount.prototype.getAccountHistory = function() {
+  return this.result;
 };
 
 module.exports = BankAccount;
