7f86764e1955a522f0a8030e091e562d02d486d9
Roma <roman.mykhalchuk@yahoo.com>
2020-04-05 20:03:32 +0300

    solution

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,28 @@
 'use strict';
 
-function BankAccount() {
-  // Write your code here
+function BankAccount(name, amount, dob) {
+  this.name = name;
+  this.amount = amount;
+  this.dob = dob;
+  this.transactionHistory = [`Initial: ${amount}`];
+}
+
+BankAccount.prototype.getInfo = function() {
+  return `Name: ${this.name}, Amount: ${this.amount}$`;
+};
+
+BankAccount.prototype.addMoney = function(sum, transactionDescr) {
+  this.amount += sum;
+  this.transactionHistory.push(`${transactionDescr}: ${sum}`);
+};
+
+BankAccount.prototype.withdrawMoney = function(sum, transactionDescr) {
+  this.amount -= sum;
+  this.transactionHistory.push(`${transactionDescr}: -${sum}`);
+};
+
+BankAccount.prototype.getAccountHistory = function() {
+  return this.transactionHistory;
 };
 
 module.exports = BankAccount;
