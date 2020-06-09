c639657955712e7ad7be96dd97bb449419824616
denkondratiev <denys.kondratiev.job@gmail.com>
2020-06-09 19:46:44 +0300

    better

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -4,26 +4,28 @@ function BankAccount(name, amount) {
   this.name = name;
   this.amount = amount;
   this.accountHistory = [`Initial: ${this.amount}`];
+};
 
-  BankAccount.prototype.getInfo = () => {
+BankAccount.prototype = {
+  getInfo: function() {
     return (`Name: ${this.name}, Amount: ${this.amount}$`);
-  };
+  },
 
-  BankAccount.prototype.addMoney = (plusAmount, info) => {
+  addMoney: function(plusAmount, info) {
     this.amount += plusAmount;
 
     this.accountHistory.push(`${info}: ${plusAmount}`);
-  };
+  },
 
-  BankAccount.prototype.withdrawMoney = (minusAmount, info) => {
+  withdrawMoney: function(minusAmount, info) {
     this.amount -= minusAmount;
 
     this.accountHistory.push(`${info}: -${minusAmount}`);
-  };
+  },
 
-  BankAccount.prototype.getAccountHistory = () => {
+  getAccountHistory: function() {
     return this.accountHistory;
-  };
+  },
 };
 
 module.exports = BankAccount;
