47472bc2b84fc5b827fe3dc0da9633504d1015cb
solaryasha <solaryasha@gmail.com>
2020-06-09 19:35:26 +0300

    My first prototype constructor

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,30 @@
 'use strict';
 
-function BankAccount() {
-  // Write your code here
+function BankAccount(name, income) {
+  this.name = name;
+  this.income = income;
+  this.history = [`Initial: ${this.income}`];
+}
+
+BankAccount.prototype = {
+  getInfo: function() {
+    return `Name: ${this.name}, Amount: ${this.income}$`;
+  },
+
+  addMoney: function(amount, operation) {
+    this.income += amount;
+    this.history.push(`${operation}: ${amount}`);
+  },
+
+  withdrawMoney: function(amount, operation) {
+    this.income -= amount;
+    this.history.push(`${operation}: ${-amount}`);
+  },
+
+  getAccountHistory: function() {
+    return this.history;
+  },
+
 };
 
 module.exports = BankAccount;
