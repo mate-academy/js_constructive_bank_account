4001a12b6584cbebba4811f88bdbdddc62411288
DTR <t.terry.rice@gmail.com>
2020-06-09 17:44:25 +0300

    final 1.0

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,29 @@
 'use strict';
 
-function BankAccount() {
-  // Write your code here
+function BankAccount(name, money) {
+  this.Name = name;
+  this.Amount = money;
+  this.moneyHistory = [`Initial: ${money}`];
+};
+
+BankAccount.prototype.getInfo = function() {
+  return `Name: ${this.Name}, Amount: ${this.Amount}$`;
+};
+
+BankAccount.prototype.getAccountHistory = function() {
+  return this.moneyHistory;
+};
+
+BankAccount.prototype.addMoney = function(sum, reason) {
+  this.Amount += sum;
+
+  return this.moneyHistory.push(`${reason}: ${sum}`);
+};
+
+BankAccount.prototype.withdrawMoney = function(sum, reason) {
+  this.Amount -= sum;
+
+  return this.moneyHistory.push(`${reason}: -${sum}`);
 };
 
 module.exports = BankAccount;
