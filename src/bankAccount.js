febcaf18acf38bb524a126b69d0aece581b01044
Glib Rulev <velzepooz@gmail.com>
2020-06-09 12:45:04 +0300

    Solution

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,30 @@
 'use strict';
 
-function BankAccount() {
-  // Write your code here
+function BankAccount(name, balance, birthdate) {
+  this.name = name;
+  this.dateOfBirth = birthdate;
+  this.balance = balance;
+  this.accountHistory = [`Initial: ${this.balance}`];
+};
+
+BankAccount.prototype = {
+  getInfo: function() {
+    return `Name: ${this.name}, Amount: ${this.balance}$`;
+  },
+
+  addMoney: function(amount, transactionName) {
+    this.accountHistory.push(`${transactionName}: ${amount}`);
+    this.balance = this.balance + amount;
+  },
+
+  withdrawMoney: function(amount, transactionName) {
+    this.accountHistory.push(`${transactionName}: ${-amount}`);
+    this.balance = this.balance - amount;
+  },
+
+  getAccountHistory: function() {
+    return this.accountHistory;
+  },
 };
 
 module.exports = BankAccount;
