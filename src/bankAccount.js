2a7c0e1fd785d98ff8ecd9abc3120cc15c69ec13
tyooma <flying.alien.godzilla@gmail.com>
2020-06-10 16:37:37 +0300

    Bank Acc solution

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,31 @@
 'use strict';
 
-function BankAccount() {
-  // Write your code here
+const operations = {
+  getInfo() {
+    return (`Name: ${this.name}, Amount: ${this.amountOfMoney}$`);
+  },
+
+  addMoney(money, transactionInfo) {
+    this.amountOfMoney += money;
+    this.history.push(`${transactionInfo}: ${money}`);
+  },
+
+  withdrawMoney(money, transactionInfo) {
+    this.amountOfMoney -= money;
+    this.history.push(`${transactionInfo}: -${money}`);
+  },
+
+  getAccountHistory() {
+    return this.history;
+  },
+};
+
+BankAccount.prototype = operations;
+
+function BankAccount(name, amountOfMoney) {
+  this.name = name;
+  this.amountOfMoney = amountOfMoney;
+  this.history = [`Initial: ${amountOfMoney}`];
 };
 
 module.exports = BankAccount;
