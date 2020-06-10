dce8188ac4585c9d33670383477c2f3f80d93f98
Andrii Mishtal <mishtal.andriy@gmail.com>
2020-06-10 09:40:49 +0300

    Solution

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -6,24 +6,26 @@ function BankAccount(name, amount) {
   this.history = [`Initial: ${this.amount}`];
 };
 
-BankAccount.prototype.getInfo = function() {
-  return `Name: ${this.name}, Amount: ${this.amount}$`;
-};
+BankAccount.prototype = {
+  getInfo() {
+    return `Name: ${this.name}, Amount: ${this.amount}$`;
+  },
 
-BankAccount.prototype.addMoney = function(money, target) {
-  this.target = target;
-  this.amount += money;
-  this.history.push(`${this.target}: ${money}`);
-};
+  addMoney(money, target) {
+    this.target = target;
+    this.amount += money;
+    this.history.push(`${this.target}: ${money}`);
+  },
 
-BankAccount.prototype.withdrawMoney = function(money, target) {
-  this.target = target;
-  this.amount -= money;
-  this.history.push(`${this.target}: -${money}`);
-};
+  withdrawMoney(money, target) {
+    this.target = target;
+    this.amount -= money;
+    this.history.push(`${this.target}: -${money}`);
+  },
 
-BankAccount.prototype.getAccountHistory = function() {
-  return this.history;
+  getAccountHistory() {
+    return this.history;
+  },
 };
 
 module.exports = BankAccount;
