9b394e57396ddfaebb3eabc9e2c448bbaa3b79c1
Slava Polishchuk <dizerllc@gmail.com>
2020-06-10 16:06:31 +0300

    shorter prototype methods declaration

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -7,22 +7,24 @@ function BankAccount(name, initialValue, dateOfBirth) {
   this.history = [`Initial: ${initialValue}`];
 };
 
-BankAccount.prototype.getInfo = function() {
-  return `Name: ${this.name}, Amount: ${this.amount}$`;
-};
+BankAccount.prototype = {
+  getInfo() {
+    return `Name: ${this.name}, Amount: ${this.amount}$`;
+  },
 
-BankAccount.prototype.addMoney = function(value, source) {
-  this.amount += value;
-  this.history.push(`${source}: ${value}`);
-};
+  addMoney(value, source) {
+    this.amount += value;
+    this.history.push(`${source}: ${value}`);
+  },
 
-BankAccount.prototype.withdrawMoney = function(value, purpose) {
-  this.amount -= value;
-  this.history.push(`${purpose}: -${value}`);
-};
+  withdrawMoney(value, purpose) {
+    this.amount -= value;
+    this.history.push(`${purpose}: -${value}`);
+  },
 
-BankAccount.prototype.getAccountHistory = function() {
-  return this.history;
+  getAccountHistory() {
+    return this.history;
+  },
 };
 
 module.exports = BankAccount;
