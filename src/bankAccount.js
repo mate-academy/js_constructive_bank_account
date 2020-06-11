0a2774ca4b4defef17596df3f1ce99e7ba1e3dd7
tyooma <flying.alien.godzilla@gmail.com>
2020-06-11 12:36:10 +0300

    Fixed solution

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,26 +1,22 @@
 'use strict';
 
-const operations = {
-  getInfo() {
-    return (`Name: ${this.name}, Amount: ${this.amountOfMoney}$`);
-  },
-
-  addMoney(money, transactionInfo) {
-    this.amountOfMoney += money;
-    this.history.push(`${transactionInfo}: ${money}`);
-  },
-
-  withdrawMoney(money, transactionInfo) {
-    this.amountOfMoney -= money;
-    this.history.push(`${transactionInfo}: -${money}`);
-  },
-
-  getAccountHistory() {
-    return this.history;
-  },
+BankAccount.prototype.getInfo = function() {
+  return (`Name: ${this.name}, Amount: ${this.amountOfMoney}$`);
 };
 
-BankAccount.prototype = operations;
+BankAccount.prototype.addMoney = function(money, transactionInfo) {
+  this.amountOfMoney += money;
+  this.history.push(`${transactionInfo}: ${money}`);
+};
+
+BankAccount.prototype.withdrawMoney = function(money, transactionInfo) {
+  this.amountOfMoney -= money;
+  this.history.push(`${transactionInfo}: -${money}`);
+};
+
+BankAccount.prototype.getAccountHistory = function() {
+  return this.history;
+};
 
 function BankAccount(name, amountOfMoney) {
   this.name = name;
